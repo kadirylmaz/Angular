88cb312c2228b6c3b31f2a421cbc496c3070066b
Kadir Yılmaz <kadirylmaz02@gmail.com>
2020-02-15 22:24:49 +0300

    ProductComponent

--- intro/src/app/app-routing.module.ts
+++ intro/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products', component : ProductComponent},
+  {path:'', redirectTo: 'products', pathMatch:'full'},
+  {path:'category', component:CategoryComponent},
+  {path:'products/category/:categoryId',component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
