fd19b1de161b5615bb634f5c5fcbde615453c13d
Kadir Yılmaz <kadirylmaz02@gmail.com>
2020-02-15 17:08:40 +0300

    Projenin oluşturulması

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -1,12 +1,18 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CategoryComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
